e68084fafa42a7bd2dd9d781fcbb536e5a25de77
Doombox <Aaron.j.king.95@gmail.com>
2022-03-15 21:34:19 +0000

    Update TODO list.

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,3 +2,8 @@
 global using System.Collections.Generic;
 
 // TODO: Re-implement Character/Roster checklists as a single reusable control.
+// TODO: Finish up the character checklist - Test that data isn't still being overwritten.
+// TODO: Modify UserData service to handle multiple discrete datatypes - prevent UserData monolith
+// TODO: Implement app settings (Timezone/Server/App Theme)
+// TODO: Implement Server Status views - Service already implemented
+// TODO: Implement faceting calculator from the ground up.
