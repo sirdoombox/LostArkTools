065e1e616fa01fb400f4cc30b2cc5947e2cd82c4
Doombox <Aaron.j.king.95@gmail.com>
2022-03-16 17:39:09 +0000

    Update TODO

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿global using Stylet;
 global using System.Collections.Generic;
 
+// TODO: Implement time check for starting up app after reset has occurred.
 // TODO: Implement app settings (Timezone/Server/App Theme)
 // TODO: Implement Server Status views - Service already implemented
 // TODO: Implement faceting calculator from the ground up.
