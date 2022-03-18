a466184c8b79da36c03f6d64ee64f2258768f0fc
Doombox <Aaron.j.king.95@gmail.com>
2022-03-18 10:55:56 +0000

    Update TODO

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,8 @@ global using System.Collections.Generic;
 
 // TODO: Checklist - Progress bar for each checklist
 // TODO: Checklist - Reorderable tasks + characters
+// TODO: Checklist - Reimplement Checklists as a single control with bindable collections to improve performance.
+// TODO: Checklist - Try to get virtualisation working to improve performance.
 // TODO: ServerStatus - Layout + Functionality improvements
 // TODO: Faceting calculator from the ground up.
 // TODO: Engraving searcher from the ground up.
